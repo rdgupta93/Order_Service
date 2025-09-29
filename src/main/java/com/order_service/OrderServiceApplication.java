632027cb3db0cc8f1adf8f965d1d1e74fc65e90b
rdgupta93 <rdgupta1336@gmail.com>
2025-09-29 15:56:02 +0530

    New Changes

--- src/main/java/com/order_service/OrderServiceApplication.java
+++ src/main/java/com/order_service/OrderServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients(basePackages = "com.order_service.feign")
 @SpringBootApplication
+
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/order_service/feign/PaymentFallback.java
+++ src/main/java/com/order_service/feign/PaymentFallback.java
@@ -3,6 +3,8 @@ package com.order_service.feign;
 public class PaymentFallback implements PaymentFeignClient {
     @Override
     public String processPayment(String orderId) {
+        System.out.println("BHai Payment Service Down Hai ");
         return "Payment Service is down, please try again later";
+
     }
 }
